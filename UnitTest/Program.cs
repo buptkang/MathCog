a9595be824d6a4270e7beb6446f002ff1710cbb5
buptkang <buptkang@gmail.com>
2014-07-04 23:06:55 -0700

    commit

---
+++ UnitTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.FSharp.Text.Lexing;
+
+namespace ParseUnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string expr = "1+3";
+            var lexbuff = LexBuffer<char>.FromString(expr);
+
+            
+        }
+    }
+}
